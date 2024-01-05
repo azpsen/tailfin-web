b068bfd3bbd3c2a2a561e43d5bc2811b24f9ce01
april <aprl.petersen@gmail.com>
2024-01-05 13:36:39 -0600

    Fix flight list ordering

--- app/routes/logbook.flights/flights-list.tsx
+++ app/routes/logbook.flights/flights-list.tsx
@@ -27,7 +27,7 @@ function useFlights() {
   const flights = useQuery({
     queryKey: ["flights-list"],
     queryFn: async () =>
-      await client.get(`/flights/by-date`).then((res) => res.data),
+      await client.get(`/flights/by-date?order=1`).then((res) => res.data),
   });
 
   return flights;
@@ -62,104 +62,116 @@ function FlightsListDisplay({
   return (
     <>
       {flights.data ? (
-        Object.entries(flights.data).map(([year, months]) => (
-          <>
-            <NavLink
-              key={randomId()}
-              label={`-- ${year} --`}
-              fw={700}
-              style={{ textAlign: "center" }}
-              defaultOpened
-              childrenOffset={0}
-            >
-              <>
-                <Divider />
-                {Object.entries(months).map(([month, days]) => (
-                  <NavLink
-                    key={randomId()}
-                    label={monthNames[Number(month) - 1]}
-                    fw={500}
-                    style={{ textAlign: "center" }}
-                    defaultOpened
-                  >
-                    <Divider />
-                    {Object.entries(days).map(([, logs]) => (
-                      <>
-                        {logs.map((flight: FlightConciseSchema) => (
-                          <>
-                            <NavLink
-                              key={randomId()}
-                              component={Link}
-                              to={`/logbook/flights/${flight.id}`}
-                              label={
-                                <Group>
-                                  <Badge
-                                    color="gray"
-                                    size="lg"
-                                    radius="sm"
-                                    px="xs"
-                                  >
-                                    {flight.date}
-                                  </Badge>
-                                  <Text fw={500}>
-                                    {`${Number(flight.time_total).toFixed(
-                                      1
-                                    )} hr`}
-                                  </Text>
-                                  {flight.waypoint_from ||
-                                  flight.waypoint_to ? (
-                                    <Text>/</Text>
-                                  ) : null}
-                                  <Group gap="xs">
-                                    {flight.waypoint_from ? (
-                                      <Text>{flight.waypoint_from}</Text>
-                                    ) : (
-                                      ""
-                                    )}
-                                    {flight.waypoint_from &&
-                                    flight.waypoint_to ? (
-                                      <IconArrowRightTail />
-                                    ) : null}
-                                    {flight.waypoint_to ? (
-                                      <Text>{flight.waypoint_to}</Text>
-                                    ) : (
-                                      ""
-                                    )}
-                                  </Group>
-                                </Group>
-                              }
-                              description={
-                                <Text lineClamp={1}>
-                                  {flight.comments
-                                    ? flight.comments
-                                    : "(No Comment)"}
-                                </Text>
-                              }
-                              rightSection={
-                                flight.aircraft ? (
-                                  <Badge
-                                    key={randomId()}
-                                    leftSection={<IconPlaneTilt size="1rem" />}
-                                    color="gray"
-                                    size="lg"
-                                  >
-                                    {flight.aircraft}
-                                  </Badge>
-                                ) : null
-                              }
-                              active={page === flight.id}
-                            />
-                            <Divider />
-                          </>
-                        ))}
-                      </>
+        Object.entries(flights.data)
+          .reverse()
+          .map(([year, months]) => (
+            <>
+              <NavLink
+                key={randomId()}
+                label={`-- ${year} --`}
+                fw={700}
+                style={{ textAlign: "center" }}
+                defaultOpened
+                childrenOffset={0}
+              >
+                <>
+                  <Divider />
+                  {Object.entries(months)
+                    .reverse()
+                    .map(([month, days]) => (
+                      <NavLink
+                        key={randomId()}
+                        label={monthNames[Number(month) - 1]}
+                        fw={500}
+                        style={{ textAlign: "center" }}
+                        defaultOpened
+                      >
+                        <Divider />
+                        {Object.entries(days)
+                          .reverse()
+                          .map(([, logs]) => (
+                            <>
+                              {logs
+                                .reverse()
+                                .map((flight: FlightConciseSchema) => (
+                                  <>
+                                    <NavLink
+                                      key={randomId()}
+                                      component={Link}
+                                      to={`/logbook/flights/${flight.id}`}
+                                      label={
+                                        <Group>
+                                          <Badge
+                                            color="gray"
+                                            size="lg"
+                                            radius="sm"
+                                            px="xs"
+                                          >
+                                            {flight.date}
+                                          </Badge>
+                                          <Text fw={500}>
+                                            {`${Number(
+                                              flight.time_total
+                                            ).toFixed(1)} hr`}
+                                          </Text>
+                                          {flight.waypoint_from ||
+                                          flight.waypoint_to ? (
+                                            <Text>/</Text>
+                                          ) : null}
+                                          <Group gap="xs">
+                                            {flight.waypoint_from ? (
+                                              <Text>
+                                                {flight.waypoint_from}
+                                              </Text>
+                                            ) : (
+                                              ""
+                                            )}
+                                            {flight.waypoint_from &&
+                                            flight.waypoint_to ? (
+                                              <IconArrowRightTail />
+                                            ) : null}
+                                            {flight.waypoint_to ? (
+                                              <Text>{flight.waypoint_to}</Text>
+                                            ) : (
+                                              ""
+                                            )}
+                                          </Group>
+                                        </Group>
+                                      }
+                                      description={
+                                        <Text lineClamp={1}>
+                                          {flight.comments
+                                            ? flight.comments
+                                            : "(No Comment)"}
+                                        </Text>
+                                      }
+                                      rightSection={
+                                        flight.aircraft ? (
+                                          <Badge
+                                            key={randomId()}
+                                            leftSection={
+                                              <IconPlaneTilt size="1rem" />
+                                            }
+                                            color="gray"
+                                            size="lg"
+                                          >
+                                            {flight.aircraft}
+                                          </Badge>
+                                        ) : null
+                                      }
+                                      active={page === flight.id}
+                                    />
+                                    <Divider />
+                                  </>
+                                ))}
+                            </>
+                          ))}
+                      </NavLink>
                     ))}
-                  </NavLink>
-                ))}
-              </>
-            </NavLink>
-          </>
-        ))
+                </>
+              </NavLink>
+            </>
+          ))
       ) : flights.isLoading ? (
         <Center h="calc(100vh - 95px - 50px)">
           <Loader />
