3407d19590040d1617da69f800d2ab02b4d5f1bd
april <aprl.petersen@gmail.com>
2024-01-08 10:28:06 -0600

    Redirect from root to dashboard/login

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,18 @@
-import { Outlet } from "@remix-run/react";
+import { useAuth } from "@/util/auth";
+import { Outlet, useNavigate } from "@remix-run/react";
+import { useEffect } from "react";
 
 export default function Tailfin() {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/login");
+    } else {
+      navigate("/logbook");
+    }
+  }, [user, loading, navigate]);
+
   return <Outlet />;
 }
